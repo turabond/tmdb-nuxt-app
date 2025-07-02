9bd0b560dbb69db4fd0ddeb1f2e9993478b32c69
sturaboiev <sturaboiev@intersog.com>
2025-07-02 09:55:09 +0300

    Updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,11 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: '2025-05-15',
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
   eslint: {
     config: {
       stylistic: {
